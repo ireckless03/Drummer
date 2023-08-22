3fb4191a3d1569cbab894b7c42485011a7d2682d
Denny Doan <denny-333@hotmail.com>
2023-08-22 13:53:40 -0400

    Added images to each drum

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let numberOfDrumButtons = document.querySelectorAll(".drum").length
 // find first button in doc and adds event listener to button
-for (let i = 0; i < numberOfDrumButtons; i++)
+for (let i = 0; i < numberOfDrumButtons; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener('click', function () {
     alert('I got clicked!');
+    
+    // What to do when click detected
+    let audio = new Audio("sounds/tom-1.mp3")
+    audio.play();
+  
   });
-
-// What to do when click detected
+}
