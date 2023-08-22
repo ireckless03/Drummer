c99e71acb5ac0ca3254594f830e8598743ac1ebc
Denny Doan <denny-333@hotmail.com>
2023-08-22 14:31:43 -0400

    Added eventListener to keys to play sound

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
+// Get the total number of buttons with class "drum"
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
-// find first button in doc and add event listener to the button
+
+// Add click event listeners to the drum buttons
 for (let i = 0; i < numberOfDrumButtons; i++) {
-
+  // Detect button click
   document.querySelectorAll(".drum")[i].addEventListener('click', function () {
-
+    // Get the clicked button's text
     let buttonInnerHTML = this.innerHTML;
-    // What to do when a click is detected
-    switch (buttonInnerHTML) {
-
-      case 'w':
-        let tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case 'a':
-        let tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case 's':
-        let tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case 'd':
-        let tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      case 'j':
-        let crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case 'k':
-        let kickbass = new Audio("sounds/kick-bass.mp3");
-        kickbass.play();
-        break;
-
-      case 'l':
-        let snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-
-      default:
-      // Handle any other cases or do nothing
-    }
-
+    // Play the corresponding sound
+    makeSound(buttonInnerHTML);
   });
 }
 
+// Check for key press events
+document.addEventListener('keypress', function (event) {
+  // Play a sound based on the pressed key
+  makeSound(event.key);
+});
+
+// Play a sound according to the given key
+function makeSound(key) {
+  switch (key) {
+    case 'w':
+      playSound("sounds/tom-1.mp3");
+      break;
+    case 'a':
+      playSound("sounds/tom-2.mp3");
+      break;
+    case 's':
+      playSound("sounds/tom-3.mp3");
+      break;
+    case 'd':
+      playSound("sounds/tom-4.mp3");
+      break;
+    case 'j':
+      playSound("sounds/crash.mp3");
+      break;
+    case 'k':
+      playSound("sounds/kick-bass.mp3");
+      break;
+    case 'l':
+      playSound("sounds/snare.mp3");
+      break;
+    default:
+    // Ignore other keys
+  }
+}
+
+// Play an audio file
+function playSound(soundFile) {
+  let audio = new Audio(soundFile);
+  audio.play();
+}
