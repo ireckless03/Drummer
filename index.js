231f56d7bd1b52afd74377df36c71476ca9e38ea
Denny Doan <denny-333@hotmail.com>
2023-08-23 14:30:26 -0400

    Refactored code

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
 $(document).ready(function () {
-  // Attach click event handlers to elements with the class 'drum'
+  const soundMap = {
+    w: 'sounds/tom-1.mp3',
+    a: 'sounds/tom-2.mp3',
+    s: 'sounds/tom-3.mp3',
+    d: 'sounds/tom-4.mp3',
+    j: 'sounds/crash.mp3',
+    k: 'sounds/kick-bass.mp3',
+    l: 'sounds/snare.mp3',
+  };
+
   $('.drum').click(function () {
-    const buttonInnerHTML = $(this).data('key');
-    makeSound(buttonInnerHTML);
-    buttonAnimation(buttonInnerHTML);
+    const buttonKey = $(this).data('key');
+    playSound(soundMap[buttonKey]);
+    buttonAnimation(buttonKey);
   });
 
-  // Attach keypress event handler to the entire document
   $(document).keypress(function (event) {
     const key = String.fromCharCode(event.which).toLowerCase();
-    makeSound(key);
-    buttonAnimation(key);
-  });
-
-  function makeSound(key) {
-    const soundMap = {
-      'w': 'sounds/tom-1.mp3',
-      'a': 'sounds/tom-2.mp3',
-      's': 'sounds/tom-3.mp3',
-      'd': 'sounds/tom-4.mp3',
-      'j': 'sounds/crash.mp3',
-      'k': 'sounds/kick-bass.mp3',
-      'l': 'sounds/snare.mp3'
-    };
-
     const soundFile = soundMap[key];
     if (soundFile) {
       playSound(soundFile);
+      buttonAnimation(key);
     }
-  }
+  });
 
   function playSound(soundFile) {
-    const audio = new Audio(soundFile);
-    audio.play();
+    new Audio(soundFile).play();
   }
 
   function buttonAnimation(currentKey) {
@@ -40,9 +33,7 @@ $(document).ready(function () {
 
     if (activeButton.length) {
       activeButton.addClass('pressed');
-      setTimeout(function () {
-        activeButton.removeClass('pressed');
-      }, 100);
+      setTimeout(() => activeButton.removeClass('pressed'), 100);
     }
   }
 });
