2f5f4f426fc0c24c5ea3a9cc661bf1472bf94cae
Denny Doan <denny-333@hotmail.com>
2023-08-23 12:34:41 -0400

    Removed comments

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-// Get the total number of buttons with class "drum"
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-// Add click event listeners to the drum buttons
 for (let i = 0; i < numberOfDrumButtons; i++) {
-  // Detect button click
-  document.querySelectorAll(".drum")[i].addEventListener('click', function () {
-    // Get the clicked button's text
-    let buttonInnerHTML = this.innerHTML;
-    // Play the corresponding sound
-    makeSound(buttonInnerHTML);
 
-    // Trigger button animation
+  document.querySelectorAll(".drum")[i].addEventListener('click', function () {
+    let buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
     buttonAnimation(buttonInnerHTML);
   });
 }
 
-// Check for key press events
 document.addEventListener('keypress', function (event) {
-  // Play a sound based on the pressed key
   makeSound(event.key);
-
-  // Trigger button animation for key press
   buttonAnimation(event.key);
 });
 
-// Play a sound according to the given key
 function makeSound(key) {
   switch (key) {
     case 'w':
@@ -49,26 +38,19 @@ function makeSound(key) {
       playSound("sounds/snare.mp3");
       break;
     default:
-      // Ignore other keys
   }
 }
 
-// Play an audio file
 function playSound(soundFile) {
   let audio = new Audio(soundFile);
   audio.play();
 }
 
-// Function to handle button animation
 function buttonAnimation(currentKey) {
-  // Select the element with the class corresponding to the currentKey
   let activeButton = document.querySelector("." + currentKey);
 
   if (activeButton) {
-    // Add the "pressed" class
     activeButton.classList.add("pressed");
-
-    // Remove the "pressed" class after a short delay (e.g., 100 milliseconds)
     setTimeout(function () {
       activeButton.classList.remove("pressed");
     }, 100);
